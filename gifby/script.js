2c437ead8b3313095405bf41ed8cf1c0469e75d8
Eric Kuecks <ekuecks@ucla.edu>
2016-05-21 14:30:25 -0700

    Can click and fill by id

--- gifby/script.js
+++ gifby/script.js
@@ -1,5 +1,5 @@
 function change() {
-  var elem = document.getElementById("example");
+  var elem = document.getElementById("example_button");
   if(elem.innerHTML == "IT DIDN'T WORK") {
     elem.innerHTML = "IT WORKED";
   }
