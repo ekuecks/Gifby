df9e63ab49392da4dd6259ae522efca4334f4f28
Eric Kuecks <ekuecks@ucla.edu>
2016-05-29 03:57:20 -0700

    consol.log's valid TOBY

--- main.js
+++ main.js
@@ -36,10 +36,68 @@ $('input').click(function(e) {
     // Keep track that this form is selected
     _selectedForm = e.target;
     console.log(e.target);
+    if(e.target.id != undefined) {
+      // Unique id for this object
+      console.log("FILL \"ID: " + e.target.id +"\" \"\"");
+    }
+    else if(e.target.className != undefined) {
+      var i = 0;
+      var nodes = $("." + e.target.className);
+      var length = nodes.length;
+      while(i < length) {
+        if(nodes.get(i) == e.target) {
+          break;
+        }
+        i++;
+      }
+      console.log("FILL \"CLASS: " + e.target.className +" NUMBER: " + i + "\" \"\"");
+    }
+    else {
+      // nodeName = INPUT
+      var i = 0;
+      var nodes = $("." + e.target.className);
+      var length = nodes.length;
+      while(i < length) {
+        if(nodes.get(i) == e.target) {
+          break;
+        }
+        i++;
+      }
+      console.log("FILL \"ATTRIBUTE: " + e.target.nodeName +" NUMBER: " + i + "\" \"\"");
+    }
 });
 
 $('button').click(function(e) {
     console.log(e.target);
+    if(e.target.id != undefined) {
+      // Unique id for this object
+      console.log("CLICK \"ID: " + e.target.id +"\"");
+    }
+    else if(e.target.className != undefined) {
+      var i = 0;
+      var nodes = $("." + e.target.className);
+      var length = nodes.length;
+      while(i < length) {
+        if(nodes.get(i) == e.target) {
+          break;
+        }
+        i++;
+      }
+      console.log("CLICK \"CLASS: " + e.target.className +" NUMBER: " + i + "\"");
+    }
+    else {
+      // nodeName = BUTTON
+      var i = 0;
+      var nodes = $("." + e.target.className);
+      var length = nodes.length;
+      while(i < length) {
+        if(nodes.get(i) == e.target) {
+          break;
+        }
+        i++;
+      }
+      console.log("CLICK \"ATTRIBUTE: " + e.target.nodeName +" NUMBER: " + i + "\"");
+    }
 });
 
 $('html').keydown( function(e) {
