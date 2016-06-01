57a90215ba79db822d0d3c4f2be67c7ec1d1a78d
Eric Kuecks <ekuecks@ucla.edu>
2016-06-01 15:39:13 -0700

    Implemented SELECT toby command

--- gifby/classes.js
+++ gifby/classes.js
@@ -107,3 +107,46 @@ class FillByAttribute extends Stmt {
     }
   }
 }
+
+class SelectByID extends Stmt {
+  constructor(id, text) {
+    super();
+    // id = "ID:id NUMBER:number
+    this.text = text.interval.contents.substring(1, text.interval.contents.length - 1);
+    this.id = id.substring(3);
+  }
+}
+
+class SelectByClass extends Stmt {
+  constructor(klass, text) {
+    super();
+    // klass = "CLASS:klass NUMBER:number
+    this.text = text.interval.contents.substring(1, text.interval.contents.length - 1);
+    var index = klass.indexOf("NUMBER:");
+    if(index > -1) {
+      this.pos = parseInt(klass.substring(index + 7)) - 1;
+      this.klass = klass.substring(6, index - 1);
+    }
+    else {
+      this.pos = 0;
+      this.klass = klass.substring(6);
+    }
+  }
+}
+
+class SelectByAttribute extends Stmt {
+  constructor(attr, text) {
+    super();
+    // attr = "ATTRIBUTE:attribute NUMBER:number
+    this.text = text.interval.contents.substring(1, text.interval.contents.length - 1);
+    var index = attr.indexOf("NUMBER:");
+    if(index > -1) {
+      this.pos = parseInt(attr.substring(index + 7)) - 1;
+      this.attr = attr.substring(10, index - 1);
+    }
+    else {
+      this.pos = 0;
+      this.attr = attr.substring(10);
+    }
+  }
+}
